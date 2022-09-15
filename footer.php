306db6aeb48ece3028258ba9f8234b784c06b18f
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-09-15 09:20:36 +0200

    footer lien

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
-    <?php wp_footer( ); ?>
+<footer>
+    <div class="container-narrow">
+        <?php if (have_rows('footer_lien', 'option')) : ?>
+            <div class="lien-footer">
+                <?php while (have_rows('footer_lien', 'option')) : the_row();
+                    $lien_footer = get_sub_field('lien');
+                    ?>
+                    <a href="<?= esc_url($lien_footer['url']); ?>"><?= esc_html($lien_footer['title']); ?></a>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; ?>
+        <p class="realiser">Réalisation: <a href="https://www.btg-communication.fr" target="_blank">Btg
+                Communication</a></p>
+        <p class="credit">&copy; Crédit Agricole</p>
+    </div>
+</footer>
+<?php wp_footer( ); ?>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -109,23 +109,6 @@ $year = get_field('annee', 'option') ? get_field('annee', 'option') : '2022';
             <?php endif; ?>
         </div>
     </section>
-    <footer>
-        <div class="container-narrow">
-            <?php if (have_rows('footer_lien', 'option')) : ?>
-                <div class="lien-footer">
-                    <?php while (have_rows('footer_lien', 'option')) : the_row();
-                        $lien_footer = get_sub_field('lien');
-                        ?>
-                        <a href="<?= esc_url($lien_footer['url']); ?>"><?= esc_html($lien_footer['title']); ?></a>
-                    <?php endwhile; ?>
-                </div>
-            <?php endif; ?>
-            <p class="realiser">Réalisation: <a href="https://www.btg-communication.fr" target="_blank">Btg
-                    Communication</a></p>
-            <p class="credit">&copy; Crédit Agricole</p>
-        </div>
-    </footer>
-
     <script>
         const { createApp } = Vue;
 
